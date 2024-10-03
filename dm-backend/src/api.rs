f853cc1d1589dd50348530ee1f54b096dc7a026a
hyx <1179570770@qq.com>
2024-10-03 22:35:44 +0800

    deal error.

--- dm-backend/src/api.rs
+++ dm-backend/src/api.rs
@@ -1,7 +1,8 @@
 use axum::extract::State;
 use axum::Json;
+use dm_common::AppResult;
 
-use dm_common::{AppError, UserDetail, UserDetailCreate, UserDetailDelete, UserDetailUpdate};
+use dm_common::{UserDetail, UserDetailCreate, UserDetailDelete, UserDetailUpdate};
 
 pub async fn index() -> &'static str {
     "Hello, world!"
@@ -15,22 +16,21 @@ pub async fn list_all(State(pool): State<sqlx::PgPool>) -> Json<Vec<UserDetail>>
 pub async fn create(
     State(pool): State<sqlx::PgPool>,
     Json(user_detail_create): Json<UserDetailCreate>,
-) -> Result<Json<UserDetail>, AppError> {
+) -> AppResult<Json<UserDetail>> {
     let new_record = UserDetail::create(
         &pool,
         user_detail_create.name,
         user_detail_create.age,
         user_detail_create.salary,
     )
-    .await
-    .unwrap();
+    .await?;
     Ok(Json::from(new_record))
 }
 
 pub async fn update(
     State(pool): State<sqlx::PgPool>,
     Json(user_detail_update): Json<UserDetailUpdate>,
-) -> Json<UserDetail> {
+) -> AppResult<Json<UserDetail>> {
     let updated_record = UserDetail::update(
         &pool,
         user_detail_update.id,
@@ -38,17 +38,14 @@ pub async fn update(
         user_detail_update.age,
         user_detail_update.salary,
     )
-    .await
-    .unwrap();
-    Json::from(updated_record)
+    .await?;
+    Ok(Json::from(updated_record))
 }
 
 pub async fn delete_one(
     State(pool): State<sqlx::PgPool>,
     Json(user_detail_delete): Json<UserDetailDelete>,
-) -> Json<UserDetail> {
-    let deleted_record = UserDetail::delete_one(&pool, user_detail_delete.id)
-        .await
-        .unwrap();
-    Json::from(deleted_record)
+) -> AppResult<Json<UserDetail>> {
+    let deleted_record = UserDetail::delete_one(&pool, user_detail_delete.id).await?;
+    Ok(Json::from(deleted_record))
 }
